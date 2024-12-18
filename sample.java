59dd9b757198a281c685ce069a92daca6d71032d
unknown <harimugunthan2004@gmail.com>
2024-12-18 15:15:48 +0530

    sample

--- sample.java
+++ sample.java
@@ -1,10 +1,11 @@
 public class sample 
 {
-public static void main(String[] args)
-{
-int a=100;
-System.out.println(a);
-System.out.println(a);
+    public static void main(String[] args)
+    {
+        int a=100;
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a + 10);
 
-}
+    }
 }
