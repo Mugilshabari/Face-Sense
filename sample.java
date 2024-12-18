a50bd95cbdd0636fc71d6629b1ca03e136389c2e
unknown <harimugunthan2004@gmail.com>
2024-12-18 15:28:02 +0530

    sample

--- sample.java
+++ sample.java
@@ -7,5 +7,17 @@ public class sample
         System.out.println(a);
         System.out.println(a + 10);
 
+        // git init
+        // git clone github repo link
+        // git config -global user.name"username"
+        // git config -global user.email"email"
+        // git status
+        // git add filename
+        // git commit -m filename
+        // git push origin 
+        // git branch
+
+
+
     }
 }
